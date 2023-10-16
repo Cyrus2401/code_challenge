c1677eb0ff230bb00c57fe284f8926aca57b7a74
Cyrus2401 <cyrushessou@gmail.com>
2023-10-16 11:34:15 +0100

    descendingOrder

---
+++ descendingOrder/index.php
@@ -0,0 +1,25 @@
+<?php
+
+    function descendingOrder($number){
+
+        $array_number = str_split($number);
+
+        rsort($array_number);
+    
+        $descendingOrderNumber = "";
+    
+        foreach ($array_number as $key => $value) {
+            
+            $descendingOrderNumber .= $value;
+    
+        }
+        
+        echo $descendingOrderNumber;
+
+    }
+
+    descendingOrder(501245)
+
+?>
+
+
